450be1dd416076a30592d994a8b010a2848cdbbd
PaoloRollo <paolo.rollo1997@gmail.com>
2024-11-16 02:12:58 +0700

    feat: update onboarding.

--- app/(app)/(home)/index.tsx
+++ app/(app)/(home)/index.tsx
@@ -1,12 +1,94 @@
+import { DeflateText } from "@/components/deflate-text";
 import { usePrivy } from "@privy-io/expo";
+import { Redirect, router } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const { user } = usePrivy();
+  const { user, isReady } = usePrivy();
+
+  if (!user && isReady) {
+    return <Redirect href={"/"} />;
+  }
 
   return (
     <>
-      <SafeAreaView className="bg-[#B6BCF9] h-screen flex flex-col items-center justify-between px-[32px]"></SafeAreaView>
+      <SafeAreaView className="bg-[#B6BCF9] h-screen flex flex-col justify-between px-[24px]">
+        <TouchableOpacity onPress={() => router.push("/(app)/(home)/profile")}>
+          <View className="rounded-full h-[64px] w-[64px] bg-[#556FC5]/50 flex items-center justify-center">
+            <DeflateText text="L" className="text-[36px] text-[#3B2086]" />
+          </View>
+        </TouchableOpacity>
+        <View className="flex flex-col items-center justify-center">
+          <DeflateText
+            text="My wallet"
+            className="text-[24px] text-[#3B2086]"
+          />
+          <DeflateText
+            text="$46,490"
+            className="text-[64px] text-[#3B2086]"
+            font="BG_Bold"
+          />
+          <View className="bg-white h-[32px] rounded-full flex items-center justify-center w-[100px]">
+            <DeflateText
+              text="+54%"
+              className="text-emerald-500 text-[24px]"
+              font="BG_Bold"
+            />
+          </View>
+          <View className="flex flex-row items-center justify-center gap-x-8 mt-4">
+            <View className="flex flex-col items-center justify-center">
+              <TouchableOpacity>
+                <View className="rounded-full flex items-center justify-center">
+                  <Image
+                    source={require("@/assets/images/deposit.png")}
+                    height={54}
+                    width={54}
+                    className="h-[54px] w-[54px]"
+                  />
+                  <DeflateText
+                    text="Deposit"
+                    className="text-[16px] text-[#3B2086] tracking-normal"
+                    font="BG_Medium"
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View className="flex flex-col items-center justify-center">
+              <TouchableOpacity>
+                <View className="rounded-full flex items-center justify-center">
+                  <Image
+                    source={require("@/assets/images/withdraw.png")}
+                    height={54}
+                    width={54}
+                    className="h-[54px] w-[54px]"
+                  />
+                  <DeflateText
+                    text="Withdraw"
+                    className="text-[16px] text-[#3B2086] tracking-normal"
+                    font="BG_Medium"
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View className="bg-white rounded-[20px] w-full h-[86px] flex flex-row items-center px-5">
+          <Text className="text-[48px] mr-3">😁</Text>
+          <View className="flex flex-col">
+            <DeflateText
+              text="Safe mode"
+              className="text-[20px]"
+              font="BG_Bold"
+            />
+            <DeflateText
+              text="Low-risk saving strategy"
+              className="text-[12px] text-[#8F8F91] tracking-normal"
+              font="BG_Medium"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     </>
   );
 }
---
+++ app/(app)/(home)/profile.tsx
@@ -0,0 +1,112 @@
+import { DeflateText } from "@/components/deflate-text";
+import { usePrivy } from "@privy-io/expo";
+import { router } from "expo-router";
+import { Image, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ProfileScreen() {
+  const { user, isReady, logout } = usePrivy();
+
+  return (
+    <>
+      <SafeAreaView className="bg-[#B6BCF9] h-screen flex flex-col px-[24px]">
+        <TouchableOpacity onPress={() => router.back()}>
+          <Image
+            source={require("@/assets/images/close.png")}
+            height={36}
+            width={36}
+            className="h-[36px] w-[36px]"
+          />
+        </TouchableOpacity>
+        <View className="flex flex-col items-center justify-center">
+          <View className="rounded-full h-[90px] w-[90px] bg-[#556FC5]/50 flex items-center justify-center">
+            <DeflateText text="L" className="text-[54px] text-[#3B2086]" />
+          </View>
+          <DeflateText
+            text="Simone Staffa"
+            className="text-[36px] text-[#3B2086]"
+            font="BG_SemiBold"
+          />
+          <DeflateText
+            text="@limone"
+            className="text-[24px] text-[#556FC5]/50 mb-8"
+          />
+          <View className="h-[86px] w-full rounded-[20px] p-4 bg-[#3B2086] flex flex-row items-center gap-x-4 mb-4">
+            <Image
+              source={require("@/assets/images/invite.png")}
+              height={48}
+              width={48}
+              className="h-[48px] w-[48px]"
+            />
+            <View className="flex flex-col">
+              <DeflateText
+                text="Invite friends"
+                className="text-[24px] text-white"
+                font="BG_Bold"
+              />
+              <DeflateText
+                text="Earn $5 per invite (up to $25)"
+                className="text-[16px] text-white tracking-normal"
+              />
+            </View>
+          </View>
+        </View>
+        <View className="w-full rounded-[20px] border-4 border-[#3B2086] p-4 flex flex-col gap-y-6">
+          <View className="flex flex-row items-center gap-x-4">
+            <Image
+              source={require("@/assets/images/identity.png")}
+              height={48}
+              width={48}
+              className="h-[48px] w-[48px]"
+            />
+            <View className="flex flex-col">
+              <DeflateText
+                text="Identity Verification"
+                className="text-[24px] text-[#3B2086]"
+                font="BG_Bold"
+              />
+              <DeflateText
+                text="Verify your identity"
+                className="text-[16px] text-[#3B2086] tracking-normal"
+              />
+            </View>
+          </View>
+          <View className="flex flex-row items-center gap-x-4">
+            <Image
+              source={require("@/assets/images/banknote.png")}
+              height={48}
+              width={48}
+              className="h-[48px] w-[48px]"
+            />
+            <View className="flex flex-col">
+              <DeflateText
+                text="Bank Account"
+                className="text-[24px] text-[#3B2086]"
+                font="BG_Bold"
+              />
+              <DeflateText
+                text="Change your connected account"
+                className="text-[16px] text-[#3B2086] tracking-normal"
+              />
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={async () => {
+            logout().then(() => {
+              console.log(user);
+              router.replace("/");
+            });
+          }}
+          className="mt-auto mb-8"
+        >
+          <DeflateText
+            text="Logout"
+            className="text-[24px] text-[#3B2086] text-center"
+            font="BG_Bold"
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
