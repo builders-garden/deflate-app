1eaa3ca46b7cf5f6181c4840a798d68d2de421ff
lim0n3 <simonestaffa96@gmail.com>
2024-11-17 02:20:22 +0700

    change ref address

--- lib/biconomy.ts
+++ lib/biconomy.ts
@@ -3,10 +3,13 @@ import {
   toSmartSessionsValidator,
   smartSessionCreateActions,
   createBicoPaymasterClient,
+  CreateSessionDataParams,
+  NexusClient,
+  ParamCondition,
 } from "@biconomy/sdk";
 import { getAction } from "viem/utils";
 import { EmbeddedWalletState } from "@privy-io/expo";
-import { Chain, encodeFunctionData, getAddress, http } from "viem";
+import { Chain, encodeFunctionData, getAddress, Hex, http } from "viem";
 import { getWalletClient } from "./wallets";
 import { base, polygon } from "viem/chains";
 import { sendUserOperation } from "viem/account-abstraction";
@@ -68,10 +71,56 @@ export const initializeBiconomySmartAccount = async (
     const nexusSessionClient = nexusClient.extend(
       smartSessionCreateActions(sessionsModule)
     );
+    await createSmartSession(
+      nexusClient,
+      nexusSessionClient,
+      wallet.account?.address as `0x${string}`
+    );
     return nexusSessionClient;
   } catch (err) {
     throw err instanceof Error
       ? err
       : new Error("Failed to initialize smart account");
   }
+};
+
+export const createSmartSession = async (
+  nexusClient: NexusClient,
+  nexusSessionClient: any,
+  sessionPublicKey: `0x${string}`
+) => {
+  const BASE_USDC_ADDRESS = "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913";
+  const sessionRequestedInfo: CreateSessionDataParams[] = [
+    {
+      sessionPublicKey,
+      actionPoliciesInfo: [
+        {
+          contractAddress: BASE_USDC_ADDRESS,
+          rules: [
+            {
+              condition: ParamCondition.EQUAL,
+              offsetIndex: 0,
+              ref: "0x95F9Ee20446A0A518E761B761BA7266909BCa21e",
+              isLimited: false,
+              usage: {
+                limit: BigInt(0),
+                used: BigInt(0),
+              },
+            },
+          ],
+          functionSelector: "0x095ea7b3" as Hex,
+        },
+      ],
+    },
+  ];
+
+  const createSessionsResponse = await nexusSessionClient.grantPermission({
+    sessionRequestedInfo,
+  });
+
+  const [cachedPermissionId] = createSessionsResponse.permissionIds;
+
+  const { success } = await nexusClient.waitForUserOperationReceipt({
+    hash: createSessionsResponse.userOpHash,
+  });
 };
