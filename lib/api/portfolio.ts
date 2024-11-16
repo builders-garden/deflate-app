e753091e1b72a1ff9287666749d605cfe936d674
lim0n3 <simonestaffa96@gmail.com>
2024-11-17 01:44:34 +0700

    portfolio data

--- lib/api/portfolio.ts
+++ lib/api/portfolio.ts
@@ -1,87 +1,45 @@
 import ky from "ky";
 
 export enum ONE_INCH_TIMERANGE {
-  "1d" = "1d",
+  "1day" = "1day",
   "1week" = "1week",
   "1month" = "1month",
   "1year" = "1year",
 }
 
-export interface CurrentValueResponse {
-  data: number;
+export interface ValueChartPoint {
+  timestamp: number;
+  value_usd: number;
 }
 
-export interface ValueChartResponse {
-  data: {
-    timestamp: number;
-    value_usd: number;
-  }[];
+export interface ProfitAndLoss {
+  abs_profit_usd: number;
+  roi: number;
 }
 
-export interface ProfitAndLossResponse {
-  data: {
-    abs_profit_usd: number;
-    roi: number;
-  };
+export interface PortfolioResponse {
+  profitAndLoss: ProfitAndLoss;
+  valueChart: ValueChartPoint[];
+  currentValue: number;
 }
-
-/**
- * @dev get current balance for user on multiple chains
- * @returns {Promise<CurrentValueResponse>} the current value data
- */
-export const getCurrentValue = async ({
-  token,
-}: {
-  token: string;
-}): Promise<CurrentValueResponse> =>
-  ky
-    .get(`${process.env.EXPO_PUBLIC_BASE_API}/v1/portfolio/current-value`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .json<CurrentValueResponse>();
-
-/**
- * @dev get value chart data for user on multiple chains
- * @returns {Promise<ValueChartResponse>} the current value data
- */
-export const getValueChart = async ({
-  token,
-  timerange,
-}: {
-  token: string;
-  timerange: ONE_INCH_TIMERANGE;
-}): Promise<ValueChartResponse> =>
-  ky
-    .get(
-      `${process.env.EXPO_PUBLIC_BASE_API}/v1/portfolio/value-chart?timerange=${timerange}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    )
-    .json<ValueChartResponse>();
-
 /**
  * @dev get profit and loss data for user on multiple chains
  * @returns {Promise<ProfitAndLossResponse>} the profit and loss value data
  */
-export const getProfitAndLoss = async ({
+export const getPortfolio = async ({
   token,
   timerange,
 }: {
   token: string;
   timerange: ONE_INCH_TIMERANGE;
-}): Promise<ProfitAndLossResponse> =>
+}): Promise<PortfolioResponse> =>
   ky
     .get(
-      `${process.env.EXPO_PUBLIC_BASE_API}/v1/portfolio/profit-and-loss?timerange=${timerange}`,
+      `${process.env.EXPO_PUBLIC_BASE_API}/v1/portfolio?timerange=${timerange}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       }
     )
-    .json<ProfitAndLossResponse>();
+    .json<PortfolioResponse>();
